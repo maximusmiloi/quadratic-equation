55ffafe0e24cbacb2b99a9521d91faff374ff445
MaximusMiloi <Maximusmiloi@gmail.com>
2018-09-26 20:42:43 +0300

    Task 4. quadratic-equation.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  const [, a, b, c] = equation
+    .replace(/\s/g, "")
+    .match(/([-+]?\d+)?\*?x\^2([-+]?\d+)?\*?x([-+]?\d+)?/)
+    .map(v => parseInt(v));
+  const D = b * b - 4 * a * c;
+  if (D < 0) {
+    return [];
+  }
+  if (D === 0) {
+    return [-b / (2 * a)];
+  }
+  return [Math.round((-b - Math.sqrt(D)) / (2 * a)), Math.round((-b + Math.sqrt(D)) / (2 * a))].sort((a, b) => a- b);
 }
+
